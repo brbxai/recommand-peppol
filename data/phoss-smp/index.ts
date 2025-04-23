7b5d664edb60858ceb1925f3e3e8a7467273a4e9
Daan Lenaerts <me@daanlenaerts.com>
2025-04-23 11:51:03 +0200

    Fixed document type registration

--- data/phoss-smp/index.ts
+++ data/phoss-smp/index.ts
@@ -6,7 +6,8 @@ import { registerBusinessCard } from "./business-card";
 import { getMigrationToken } from "../hermes";
 
 const DOCUMENT_TYPE_CODES = [
-  "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1"
+  "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1",
+  "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1"
 ]
 const DOCUMENT_PROCESS_ID_CODES = [
   "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0"
