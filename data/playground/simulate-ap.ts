f6ea590c948ed1bdde334ae6ea1601bab8d483ef
Daan Lenaerts <me@daanlenaerts.com>
2025-09-02 10:10:09 +0200

    Removed playground requirement of companies being registered

--- data/playground/simulate-ap.ts
+++ data/playground/simulate-ap.ts
@@ -1,3 +1,4 @@
+import { UserFacingError } from "@peppol/utils/util";
 import { getCompanyByPeppolId } from "../companies";
 import { receiveDocument } from "../receive-document";
 
@@ -12,9 +13,20 @@ export async function simulateSendAs4(options: {
   // Playground specific options
   playgroundTeamId: string;
 }) {
+
+  // If the recipientId is "404:404", throw an error
+  if (options.receiverId === "404:404") {
+    throw new UserFacingError("This document was sent to recipient 404:404, simulating the sending of a document to a Peppol address that does not exist.");
+  }
+
   // Check if the receiverId is registered as a company in this playground team, search by enterprise number
-  const receivingCompany = await getCompanyByPeppolId(options.receiverId, options.playgroundTeamId);
-  if (!receivingCompany || receivingCompany.teamId !== options.playgroundTeamId) {
+  try {
+    const receivingCompany = await getCompanyByPeppolId(options.receiverId, options.playgroundTeamId);
+    if (!receivingCompany || receivingCompany.teamId !== options.playgroundTeamId) {
+      // Silently fail, the receiver is not registered in this playground team, so we don't need to send the document
+      return
+    }
+  } catch (error) {
     // Silently fail, the receiver is not registered in this playground team, so we don't need to send the document
     return
   }
@@ -30,5 +42,5 @@ export async function simulateSendAs4(options: {
     skipBilling: true,
     playgroundTeamId: options.playgroundTeamId,
   });
-  
+
 }
